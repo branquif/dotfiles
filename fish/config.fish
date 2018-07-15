ad7ed604180f04bc101bb9ed256cfc88b3857408
Fabio Branquinho <branquif@gmail.com>
2018-07-14 22:11:26 -0300

    adjusted fish files

--- fish/config.fish
+++ fish/config.fish
@@ -14,7 +14,7 @@ set -x -g XDG_RUNTIME_DIR /tmp
 #set -x -g XDG_RUNTIME_DIR
 
 # User bin folder
-set -x -g PATH $XDG_EXEC_HOME $PATH /usr/local/sbin
+set -x -g PATH $XDG_EXEC_HOME $PATH 
 
 # set ls colors
 #set -x -g LS_COLORS "di=38;5;27:fi=38;5;7:ln=38;5;51:pi=40;38;5;11:so=38;5;13:or=38;5;197:mi=38;5;161:ex=38;5;9:"
@@ -38,8 +38,9 @@ set -x -g PATH (brew --prefix findutils)/libexec/bin $PATH
 eval (pipenv --completion)
 
 # set pyenv
+# pyenv
+set -x -g PATH /Users/fabio.branquinho/.pyenv/bin $PATH
 status --is-interactive; and source (pyenv init - | psub)
-#status --is-interactive; and source (pyenv virtualenv init -|psub)
 
 # npm to use XDG
 # see npm config ls -l | grep /
